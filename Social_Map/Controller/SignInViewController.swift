f1269ad60e54b37f2691c46cdb13a9b6cfa2b74e
Dillon Cain <dilloncain@caincomputer.com>
2019-03-21 23:14:12 -0700

    Basic sign in gui complete

---
+++ Social_Map/Controller/SignInViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SignInViewController.swift
+//  Social_Map
+//
+//  Created by Dillon Cain on 3/21/19.
+//  Copyright © 2019 Cain Computers. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import GeoFire
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
