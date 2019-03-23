42813514207bb56ed58e348996f49c1cfc7bba6b
Dillon Cain <dilloncain@caincomputer.com>
2019-03-23 15:24:17 -0700

    Completed basic feed-ui with constraints

--- Social_Map/View/GreatView.swift
+++ Social_Map/View/GreatView.swift
@@ -17,7 +17,7 @@ class GreatView: UIView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        
+        layer.cornerRadius = 2.0
     }
 
 }
