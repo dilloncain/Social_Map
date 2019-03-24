a1d70a2540e617cc7d1905b3350fded528296734
Dillon Cain <dilloncain@caincomputer.com>
2019-03-23 22:54:19 -0700

    Firebase GET completed

---
+++ Model/Post.swift
@@ -0,0 +1,57 @@
+//
+//  Post.swift
+//  Social_Map
+//
+//  Created by Dillon Cain on 3/23/19.
+//  Copyright © 2019 Cain Computers. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Post {
+    private var _caption: String!
+    private var _imageUrl: String!
+    private var _likes: Int!
+    private var _postKey: String!
+    
+    var caption: String {
+     return _caption
+    }
+    
+    var imagesUrl: String {
+        return _imageUrl
+    }
+    
+    var likes: Int {
+        return _likes
+    }
+    
+    var postKey: String {
+        return _postKey
+    }
+    
+    init(caption: String, imageUrl: String, likes: Int) {
+        self._caption = caption
+        self._imageUrl = imageUrl
+        // caption in imageUrl?
+        self._likes = likes
+    }
+    
+    init(postKey: String, postData: Dictionary<String, AnyObject>) {
+        self._postKey = postKey
+        
+        if let caption = postData["caption"] as? String {
+            self._caption = caption
+        }
+        
+        if let imageUrl = postData["imageUrl"] as? String {
+            self._imageUrl = imageUrl
+        }
+        
+        if let likes = postData["likes"] as? Int {
+            self._likes = likes
+        }
+        
+    }
+}
--- Social_Map/Controller/FeedViewController.swift
+++ Social_Map/Controller/FeedViewController.swift
@@ -14,6 +14,8 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
 
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,7 +25,18 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         // Initializes listener
         DataService.ds.REF_POSTS.observe(.value) { (snapshot) in
-            print(snapshot.value)
+            //print(snapshot.value)
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         }
     }
     
@@ -32,10 +45,14 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("Dillon: \(post.caption)")
+        
         return tableView.dequeueReusableCell(withIdentifier: "PostTableViewCell") as! PostTableViewCell
     }
     
