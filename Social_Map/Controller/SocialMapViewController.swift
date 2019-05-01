9858c647a903493fe0e51a31073b9494aa785f72
Dillon Cain <dilloncain@caincomputer.com>
2019-05-01 11:25:37 -0700

    Map view controller layout

--- Social_Map/Controller/SocialMapViewController.swift
+++ Social_Map/Controller/SocialMapViewController.swift
@@ -10,50 +10,25 @@ import UIKit
 import GoogleMaps
 import GooglePlaces
 import Firebase
-//class SocialMapViewController: UIViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Do any additional setup after loading the view.
-//    }
-//
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
-//
-//}
 
 class SocialMapViewController: UIViewController, GMSMapViewDelegate {
     @IBAction func socialMapBackButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "goToSignInTwo", sender: nil)
     }
     
     
     // You don't need to modify the default init(nibName:bundle:) method.
     
-    override func loadView() {
-        
-    
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
-        
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
-        
-        
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        guard let navigationController = segue.destination as? UINavigationController,
+//            let controller = navigationController.topViewController as? TypesTableViewController else {
+//                return
+//        }
+//        controller.selectedTypes = searchedTypes
+//        controller.delegate = self
+//    }
 }
